4417796c95b381dd997e5a5137cfe748f5f8e387
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-08-05 13:02:28 +0800

    Class: SecurityGuard

---
+++ src/SecurityGuard.java
@@ -0,0 +1,9 @@
+public class SecurityGuard extends Thread{
+
+    String securityGuardName;
+
+    public SecurityGuard(String securityGuardName){
+        this.securityGuardName = securityGuardName;
+    }
+
+}
