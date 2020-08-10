317367e59e4758afd6204a2649c421381f211316
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-08-11 01:43:00 +0800

    Nothing much

--- src/TicketCounter.java
+++ src/TicketCounter.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -12,12 +13,12 @@ public class TicketCounter{
 
     public void sellTicket(Customer customer){
         if(lock.tryLock()){
-            System.out.println("("+java.time.LocalTime.now().withNano(0) + ")" + " Customer " + customer.getName() + " reached " + this.counterName);
+            System.out.println("("+java.time.LocalTime.now().withNano(0) + " - TC)" + " Customer " + customer.getName() + " reached " + this.counterName);
             customer.getTicket = true;
             try{
-                Thread.sleep(1500);
+                Thread.sleep(new Random().nextInt(3) * 2500);
             }catch(Exception e){}
-            System.out.println("("+java.time.LocalTime.now().withNano(0) + ") " + this.counterName + " sold Ticket to Customer: " + customer.getName());
+            System.out.println("("+java.time.LocalTime.now().withNano(0) + " - TC) " + this.counterName + " sold Ticket to Customer: " + customer.getName());
             lock.unlock();
         }
     }
