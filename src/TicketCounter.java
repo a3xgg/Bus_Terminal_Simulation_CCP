b90118563842c589966a0972078899acba91f95e
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-08-10 21:53:29 +0800

    Added timestamp to output message

--- src/TicketCounter.java
+++ src/TicketCounter.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -14,12 +12,12 @@ public class TicketCounter{
 
     public void sellTicket(Customer customer){
         if(lock.tryLock()){
-            System.out.println("Customer " + customer.getName() + " reached " + this.counterName);
+            System.out.println("("+java.time.LocalTime.now().withNano(0) + ")" + " Customer " + customer.getName() + " reached " + this.counterName);
             customer.getTicket = true;
             try{
                 Thread.sleep(1500);
             }catch(Exception e){}
-            System.out.println(this.counterName + " sold Ticket to Customer: " + customer.getName());
+            System.out.println("("+java.time.LocalTime.now().withNano(0) + ") " + this.counterName + " sold Ticket to Customer: " + customer.getName());
             lock.unlock();
         }
     }
