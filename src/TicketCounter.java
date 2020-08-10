eb3b22caf04b78f65ac3fb3a634123af9b36d281
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-08-10 21:43:05 +0800

    Function: Sell Ticket Function

--- src/TicketCounter.java
+++ src/TicketCounter.java
@@ -1,14 +1,26 @@
-public class TicketCounter extends Thread{
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-    Customer customer;
+public class TicketCounter{
 
-    public TicketCounter(String counterName, Customer customer){
-        setName(counterName);
-        this.customer = customer;
+    String counterName;
+    Lock lock = new ReentrantLock();
+
+    public TicketCounter(String counterName){
+        this.counterName = counterName;
     }
 
-    @Override
-    public void run(){
-
+    public void sellTicket(Customer customer){
+        if(lock.tryLock()){
+            System.out.println("Customer " + customer.getName() + " reached " + this.counterName);
+            customer.getTicket = true;
+            try{
+                Thread.sleep(1500);
+            }catch(Exception e){}
+            System.out.println(this.counterName + " sold Ticket to Customer: " + customer.getName());
+            lock.unlock();
+        }
     }
 }
