8b23b13c0004a1e46e11bba8c2ce08761cccc95e
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-08-11 15:04:14 +0800

    Tried simulate machine breakdown, managed to use the customer name instead to provide error details

--- src/TicketMachine.java
+++ src/TicketMachine.java
@@ -3,8 +3,8 @@ import java.util.Random;
 /* Ticket Machine that extends from TicketCounter */
 public class TicketMachine extends TicketCounter{
 
-    int servedCounter = 1; // this counter is used to simulate ticket machine breakdown when it reaches a certain number
-    boolean brokeDown = false;
+    private int servedCounter = 1; // this counter is used to simulate ticket machine breakdown when it reaches a certain number
+    private boolean brokeDown = false;
 
     public TicketMachine(String machineName){
         super(machineName);
@@ -12,12 +12,12 @@ public class TicketMachine extends TicketCounter{
 
     @Override
     public void sellTicket(Customer customer){
-        if(!brokeDown && servedCounter < 10){
+        if(!brokeDown && servedCounter < 15){   // set the breakdown counter be random
             if(lock.tryLock()){
                 System.out.println("#" + servedCounter);
                 System.out.println("("+java.time.LocalTime.now().withNano(0) + " - TM)" + " Customer " + customer.getName() + " reached " + this.counterName);
                 try{
-                    Thread.sleep(0);
+                    Thread.sleep(1500);
                 }catch(Exception e){}
                 System.out.println("("+java.time.LocalTime.now().withNano(0) + " - TM) " + this.counterName + " sold Ticket to Customer: " + customer.getName());
                 customer.getTicket = true;
@@ -25,8 +25,15 @@ public class TicketMachine extends TicketCounter{
                 lock.unlock();
             }
         } else {
-            customer.ticketMachine = null;
-            customer.ticket.sellTicket(customer);
+            brokeDown = true;
+            try{
+                Thread.sleep(2500);
+            } catch(Exception e){}
+            finally{
+                System.out.println("Customer " + customer.getName() + " has left to " + customer.ticket.counterName + " - Reason: " + counterName + " broke down!");
+                customer.ticketMachine = null;
+                customer.ticket.sellTicket(customer);
+            }
         }
     }
 }
