c33ca51a3843a8b2b1cc5b795dd8a2d2f65f4594
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-08-11 12:47:23 +0800

    Added ticket counter and machine selling function

--- src/Customer.java
+++ src/Customer.java
@@ -1,12 +1,40 @@
 /* Customer Thread */
 public class Customer extends Thread{
 
-    public Customer(String customerName){
+    TicketCounter ticket;
+    TicketMachine ticketMachine;
+
+    boolean getTicket = false;
+
+    public Customer(String customerName, TicketCounter ticket){
         setName(customerName);
+        this.ticket = ticket;
+    }
+
+    public Customer(String customerName, TicketMachine ticketMachine){
+        setName(customerName);
+        this.ticketMachine = ticketMachine;
+    }
+
+    public Customer(String customerName, TicketMachine ticketMachine, TicketCounter ticket){
+        this(customerName, ticketMachine);
+        this.ticket = ticket;
     }
 
     @Override
     public void run(){
-
+        while(true){
+           if(this.getTicket == false){
+               if (this.ticket != null && this.ticketMachine == null) {
+                   ticket.sellTicket(this);
+               } else if(this.ticketMachine != null && this.ticket != null){
+                   if(ticketMachine.brokeDown == false ){
+                       ticketMachine.sellTicket(this);
+                   } else {
+                       ticket.sellTicket(this);
+                   }
+               }
+           }
+        }
     }
 }
