99edcfb5ce74ab3bd67ae548af593801708be7c4
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-08-11 20:46:09 +0800

    Added waiting area class

--- src/WaitingArea.java
+++ src/WaitingArea.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class WaitingArea {
+
+    int maxCapacity;
+    boolean isFullCapacity = false;
+    Lock lock = new ReentrantLock();
+    String waitingAreaName;
+
+    public WaitingArea(String waitingAreaName){
+        this.maxCapacity = 10;
+        this.waitingAreaName = waitingAreaName;
+    }
+
+    public void enterWaitingArea(Customer customer){
+        if(lock.tryLock()){
+            System.out.println("(" + java.time.LocalTime.now().withNano(0) + " - WA) " + "Customer " + customer.getName() + " is headed towards " + waitingAreaName);
+            try{
+                Thread.sleep(0);
+            } catch(Exception e){}
+            System.out.println("(" + java.time.LocalTime.now().withNano(0) + " - WA) " + "Customer " + customer.getName() + " has reached " + waitingAreaName);
+            maxCapacity -= 1;
+            System.out.println("Current Number of available seats at " + this.waitingAreaName + ": " + maxCapacity);
+            isFullCapacity = maxCapacity <= 0 ? true: false;
+            lock.unlock();
+        }
+    }
 }
