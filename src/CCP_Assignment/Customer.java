0c63fd261b6f15a256b407ecef306a38f59f617b
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-08-14 22:32:21 +0800

    Added classes to accept busterminal and ticketcounter param

--- src/CCP_Assignment/Customer.java
+++ src/CCP_Assignment/Customer.java
@@ -1,9 +1,36 @@
+package CCP_Assignment;
+
 import CCP_Assignment.TicketCounter;
 
 public class Customer extends Thread{
 
+    TicketCounter ticketCounter;
+    BusTerminal busTerminal;
+    boolean ticket = false;
+    boolean entered = false;
+
+    public Customer(String customerName, BusTerminal busTerminal){
+        setName(customerName);
+        this.busTerminal = busTerminal;
+    }
+
+    public Customer(String customerName, TicketCounter ticketCounter){
+        setName(customerName);
+        this.ticketCounter = ticketCounter;
+    }
+
+    public Customer(String customerName, BusTerminal busTerminal, TicketCounter ticketCounter){
+        this(customerName, busTerminal);
+        this.ticketCounter = ticketCounter;
+    }
+
     @Override
     public void run(){
-
+//        while(ticket == false){
+//            ticketCounter.sellTicket(this);
+//        }
+        while(entered == false){
+            busTerminal.enterBusTerminal(this);
+        }
     }
 }
