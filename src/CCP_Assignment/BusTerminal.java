ed0ed37127293905cf9f57b0c25e2f7210e8aeea
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-08-14 22:31:18 +0800

    Function: Added enter bus terminal function

--- src/CCP_Assignment/BusTerminal.java
+++ src/CCP_Assignment/BusTerminal.java
@@ -1,17 +1,50 @@
-public class BusTerminal extends Thread{
+package CCP_Assignment;
 
-    int terminalCustomerLimit;
+import java.util.Random;
 
-    Entrance westEntrance, eastEntrance;
-    SecurityGuard westGuard, eastGuard;
+public class BusTerminal{
 
-    public BusTerminal(){
-        this.westEntrance = new Entrance("West Entrance", this.westGuard = new SecurityGuard("West Guard"));
-        this.eastEntrance = new Entrance("East Entrance", this.eastGuard = new SecurityGuard("East Guard"));
+    int maxCapacity;
+    int customerCounter = 0;
+    boolean max = false;
+    String busTerminalName;
+
+    public BusTerminal(String busTerminalName){
+        this.busTerminalName = busTerminalName;
+        this.maxCapacity = 10;
     }
 
-    @Override
-    public void run(){
+    public String getBusTerminalName(){
+        return this.busTerminalName;
+    }
 
+    public synchronized void increaseCustomerCounter(){
+        customerCounter += 1;
+    }
+
+    public void enterBusTerminal(Customer customer){
+        synchronized (this){
+            max = (customerCounter == maxCapacity) ? true : false;
+            if(!max){
+                try{
+                    Thread.sleep((new Random().nextInt(4) + 1) * 1000);
+                } catch(Exception e){}
+                customer.busTerminal.increaseCustomerCounter();
+                System.out.println("(" + java.time.LocalTime.now().withNano(0) + " - " + getBusTerminalName() + ")\t" + "Customer " + customer.getName() + " reached the terminal entrance");
+                try{
+                    Thread.sleep(1000);
+                } catch(Exception e){}
+                customer.entered = true;
+                System.out.println("(" + java.time.LocalTime.now().withNano(0) + " - " + getBusTerminalName() + ")\t" + "Customer " + customer.getName() + " has entered the bus terminal" + "(Current Capacity: " + customerCounter+")");
+            } else {
+                System.out.println("(" + java.time.LocalTime.now().withNano(0) + " - " + getBusTerminalName() + ")\t" + "Customer " + customer.getName() + " is currently waiting at the entrance");
+                try{
+                    Thread.sleep(5000);
+                } catch(Exception e){}
+            }
+        }
+        if(customer.entered && customer.ticket == false){
+            customer.ticketCounter.sellTicket(customer);
+        }
     }
 }
