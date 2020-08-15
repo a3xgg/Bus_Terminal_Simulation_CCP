767ab6a0eafcc75c84d50ed766052ac0b122efe7
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-08-16 00:41:10 +0800

    nothing much

--- src/CCP_Assignment/BusTerminal.java
+++ src/CCP_Assignment/BusTerminal.java
@@ -2,16 +2,16 @@ package CCP_Assignment;
 
 import java.util.Random;
 
-public class BusEntrance {
+public class BusTerminal {
 
     int maxCapacity;
     int customerCounter = 0;
     boolean max = false;
     String busTerminalName;
 
-    public BusEntrance(String busTerminalName){
+    public BusTerminal(String busTerminalName){
         this.busTerminalName = busTerminalName;
-        this.maxCapacity = 10;
+        this.maxCapacity = 20;
     }
 
     public String getBusEntrance(){
@@ -25,20 +25,16 @@ public class BusEntrance {
     public void enterBusTerminal(Customer customer){
         synchronized (this){
             max = (customerCounter == maxCapacity) ? true : false;
-            try{
-                Thread.sleep((new Random().nextInt(4) + 1) * 1000);
-            } catch(Exception e){}
-            System.out.println("(" + java.time.LocalTime.now().withNano(0) + " - " + getBusEntrance() + ")\t" + "Customer " + customer.getName() + " reached the terminal entrance");
             if(!max){
-
-                synchronized (this){
-                    customer.busEntrance.increaseCustomerCounter();
-                }
-
+                try{
+                    Thread.sleep((new Random().nextInt(4) + 1) * 1000);
+                } catch(Exception e){}
+                System.out.println("(" + java.time.LocalTime.now().withNano(0) + " - " + getBusEntrance() + ")\t" + "Customer " + customer.getName() + " reached the terminal entrance");
                 try{
                     Thread.sleep(1000);
                 } catch(Exception e){}
                 customer.entered = true;
+                customer.busTerminal.increaseCustomerCounter();
                 System.out.println("(" + java.time.LocalTime.now().withNano(0) + " - " + getBusEntrance() + ")\t" + "Customer " + customer.getName() + " has entered the bus terminal" + "(Current Capacity: " + customerCounter+")");
             } else {
                 System.out.println("(" + java.time.LocalTime.now().withNano(0) + " - " + getBusEntrance() + ")\t" + "Customer " + customer.getName() + " is currently waiting at the entrance");
