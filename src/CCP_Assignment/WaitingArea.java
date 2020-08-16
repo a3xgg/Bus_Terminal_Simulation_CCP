5e5566712f3791b9a41520c080280873f8b9f447
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-08-16 14:55:57 +0800

    Change class to Thread, some bugs left

--- src/CCP_Assignment/WaitingArea.java
+++ src/CCP_Assignment/WaitingArea.java
@@ -2,36 +2,60 @@ package CCP_Assignment;
 
 import java.util.Random;
 
-public class WaitingArea {
+public class WaitingArea extends Thread{
 
-    String waitingAreaName;
     boolean isFull;
+    int maxCapacity;
     int waitingAreaCustomerCounter = 0;
 
     public WaitingArea(String waitingAreaName){
-        this.waitingAreaName = waitingAreaName;
-    }
-
-    public String getWaitingAreaName(){
-        return this.waitingAreaName;
+        setName(waitingAreaName);
+        this.maxCapacity = 10;
     }
 
     public synchronized int incrementCounter(){
         return waitingAreaCustomerCounter += 1;
     }
 
+    public synchronized int decrementCounter(){
+        return waitingAreaCustomerCounter -= 1;
+    }
+
     public synchronized void enterWaitingArea(Customer customer){
-        isFull = waitingAreaCustomerCounter == 10 ? true : false;
+        isFull = waitingAreaCustomerCounter == maxCapacity ? true : false;
         if(!isFull){
             try{
                 Thread.sleep((new Random().nextInt(2) + 1) * 1000);
-                System.out.println("(" + java.time.LocalTime.now().withNano(0) + " - " + getWaitingAreaName() + ")\t" + "Customer " + customer.getName() + " has REACHED " + getWaitingAreaName());
+                System.out.println("(" + java.time.LocalTime.now().withNano(0) + " - " + getName() + ")\t" + "Customer " + customer.getName() + " has REACHED " + getName());
                 Thread.sleep(1000);
                 incrementCounter();
-                System.out.println("(" + java.time.LocalTime.now().withNano(0) + " - " + getWaitingAreaName() + ")\t" + "Customer " + customer.getName() + " has ENTERED " + getWaitingAreaName() + "\t(Current Capacity: " + waitingAreaCustomerCounter + ")");
+                System.out.println("(" + java.time.LocalTime.now().withNano(0) + " - " + getName() + ")\t" + "Customer " + customer.getName() + " has ENTERED " + getName() + "\t(Current Capacity: " + waitingAreaCustomerCounter + ")");
             } catch(Exception e){}
+//            synchronized (customer.bus){
+//                try{
+//                    customer.bus.wait();
+//                } catch(Exception e){}
+//            }
+//            customer.bus.enterBus(customer);
+//            customer.bus.incrementBusCounter();
         } else{
+            customer.foyer.enterFoyer(customer);
+        }
+    }
 
+    @Override
+    public void run(){
+        while(true){
+            if(!isFull){
+                try{
+                    synchronized (this){ this.notify();}
+                }catch(Exception e){}
+            } else {
+                System.out.println(getName() + " is FULL please proceed to the Foyer");
+                try{
+                    Thread.sleep(5000);
+                } catch(Exception e){}
+            }
         }
     }
 }
