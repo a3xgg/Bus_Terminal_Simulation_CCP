98a3c41e1e22d323604ddf42f35f9268a408c1b2
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-08-16 14:56:57 +0800

    nothing much

--- src/CCP_Assignment/BusTerminalThread.java
+++ src/CCP_Assignment/BusTerminalThread.java
@@ -12,24 +12,36 @@ public class BusTerminalThread extends Thread{
         System.out.println("WELCOME TO " + getName() + " WE ARE OPEN FOR SERVICE!");
 
         BusTerminal busTerminal = new BusTerminal("APBT");
+
+        Foyer foyer = new Foyer("Foyer");
+
         TicketCounter counterOne = new TicketCounter("Counter 1");
         TicketCounter counterTwo = new TicketCounter("Counter 2");
-        WaitingArea waitingAreaOne = new WaitingArea("Waiting Area 1");
 
-        Customer[] customers = new Customer[20];
+        WaitingArea waitingAreaOne = new WaitingArea("Waiting Area 1");
+        WaitingArea waitingAreaTwo = new WaitingArea("Waiting Area 2");
+        WaitingArea waitingAreaThree = new WaitingArea("Waiting Area 3");
+
+        Bus bus = new Bus("APBT Bus");
+        Bus bus2 = new Bus("APBT Bus 2");
+        Bus bus3 = new Bus("APBT Bus 3");
+
+        Customer[] customers = new Customer[15];
         for(int i = 0; i < customers.length; i++){
             int randomGenerator = new Random().nextInt(2);
             switch (randomGenerator){
                 case 0:
-                    customers[i] = new Customer(Integer.toString(i), busTerminal,waitingAreaOne ,counterOne);
+                    customers[i] = new Customer(Integer.toString(i), busTerminal,waitingAreaOne , bus, foyer,counterOne);
                     break;
                 case 1:
-                    customers[i] = new Customer(Integer.toString(i), busTerminal, waitingAreaOne,counterTwo);
+                    customers[i] = new Customer(Integer.toString(i), busTerminal, waitingAreaOne, bus, foyer,counterTwo);
                     break;
             }
         }
+        waitingAreaOne.start();
         for(int i = 0; i < customers.length; i++){
             customers[i].start();
         }
+
     }
 }
