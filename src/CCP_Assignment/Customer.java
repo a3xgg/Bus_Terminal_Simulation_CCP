38be9ffe88129f073542e3cb27bef80c81db8b6b
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-08-16 14:57:52 +0800

    Merged constructors

--- src/CCP_Assignment/Customer.java
+++ src/CCP_Assignment/Customer.java
@@ -5,42 +5,22 @@ public class Customer extends Thread{
     TicketCounter ticketCounter;
     BusTerminal busTerminal;
     WaitingArea waitingArea;
+    Bus bus;
     Foyer foyer;
     boolean ticket = false;
     boolean entered = false;
 
-    public Customer(String customerName, BusTerminal busTerminal){
+    public Customer(String customerName, BusTerminal busTerminal,WaitingArea waitingArea, Bus bus, Foyer foyer, TicketCounter ticketCounter){
         setName(customerName);
-        this.busTerminal = busTerminal;
-    }
-
-    public Customer(String customerName, TicketCounter ticketCounter){
-        setName(customerName);
-        this.ticketCounter = ticketCounter;
-    }
-
-    public Customer(String customerName, BusTerminal busTerminal, TicketCounter ticketCounter){
-        this(customerName, busTerminal);
-        this.ticketCounter = ticketCounter;
-    }
-
-    public Customer(String customerName, BusTerminal busTerminal,WaitingArea waitingArea , TicketCounter ticketCounter){
-        this(customerName, busTerminal);
-        this.waitingArea = waitingArea;
-        this.ticketCounter = ticketCounter;
-    }
-    public Customer(String customerName, BusTerminal busTerminal,WaitingArea waitingArea, Foyer foyer, TicketCounter ticketCounter){
-        this(customerName, busTerminal);
-        this.waitingArea = waitingArea;
-        this.ticketCounter = ticketCounter;
         this.foyer = foyer;
+        this.busTerminal = busTerminal;
+        this.bus = bus;
+        this.waitingArea = waitingArea;
+        this.ticketCounter = ticketCounter;
     }
 
     @Override
     public void run(){
-//        while(ticket == false){
-//            ticketCounter.sellTicket(this);
-//        }
         while(entered == false){
             busTerminal.enterBusTerminal(this);
         }
