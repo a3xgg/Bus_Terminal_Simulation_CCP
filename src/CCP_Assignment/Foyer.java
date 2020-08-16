b4ab117a53cbd6109357ddc856c0cabcfe29a36f
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-08-16 14:56:24 +0800

    Added Foyer class, still to work with Waiting Area

--- src/CCP_Assignment/Foyer.java
+++ src/CCP_Assignment/Foyer.java
@@ -1,12 +1,30 @@
 package CCP_Assignment;
 
-public class Foyer {
-
-    String foyerName;
+public class Foyer extends Thread{
 
     public Foyer (String foyerName){
-        this.foyerName = foyerName;
+        setName(foyerName);
     }
 
+    public void enterFoyer(Customer customer){
+        synchronized (this){
+            try {
+                Thread.sleep(1500);
+                System.out.println("Customer " + customer.getName() + " is waiting at the Foyer");
+            } catch(Exception e){}
+        }
+//        synchronized (customer.waitingArea){
+//            try{
+//                customer.waitingArea.wait();
+//            } catch(Exception e){}
+//        }
+//        customer.waitingArea.enterWaitingArea(customer);
+    }
 
+    @Override
+    public void run(){
+        while(true){
+
+        }
+    }
 }
