341523dac197a2b80b8fe86ec1ebad36c5db5e80
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-09-04 00:30:51 +0800

    change override method

--- src/CCP_Assignment/TicketMachine.java
+++ src/CCP_Assignment/TicketMachine.java
@@ -12,7 +12,7 @@ public class TicketMachine extends TicketCounter{
         super(counterName);
         this.brokeDown = false;
         //brokeDownRandomizer = (new Random().nextInt(10)+1) * 5; // max breakdown @ 50, min breakdown @ 5
-        brokeDownRandomizer = 10;
+        brokeDownRandomizer = 1;
     }
 
     @Override
@@ -23,7 +23,20 @@ public class TicketMachine extends TicketCounter{
                 System.out.println("("+java.time.LocalTime.now().withNano(0)+")" + " Customer "+ customer.getName() + " is headed towards " + customer.ticketCounter.getCounterName());
                 customer.ticketCounter.sellTicket(customer);
             } else {
-                super.sellTicket(customer);
+//                super.sellTicket(customer);
+                try{
+                        Thread.sleep(new Random().nextInt(3) * 1000);
+                    } catch(Exception e){}
+                    System.out.println("("+java.time.LocalTime.now().withNano(0) + " - " + getCounterName() + ")\t" + "Customer " + customer.getName() + " has reached " + getCounterName());
+                try{
+                    Thread.sleep(1000);
+                } catch(Exception e){}
+                customer.ticket = true;
+                System.out.println("("+java.time.LocalTime.now().withNano(0) + " - " + getCounterName() + ")\t" + getCounterName() + " has sold a ticket to Customer " + customer.getName());
+
+                if(customer.ticket == true){
+                    customer.ticketScanner.scanTicket(customer);
+                }
                 servedCounter += 1;
                 brokeDown = servedCounter == brokeDownRandomizer ? true : false;
                 if(servedCounter == brokeDownRandomizer){
