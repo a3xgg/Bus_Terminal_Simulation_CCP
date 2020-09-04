34acc2abef0f5be8ee840c917c876039d6beade1
a3xgg <39831624+a3xgg@users.noreply.github.com>
2020-09-04 13:02:56 +0800

    added toilet break scenario

--- src/CCP_Assignment/TicketCounter.java
+++ src/CCP_Assignment/TicketCounter.java
@@ -4,8 +4,14 @@ import java.util.Random;
 
 public class TicketCounter{
     private String counterName;
+
+    boolean onBreak = false;
+    int servedCounter = 0;
+    int counterToBreak;
+
     public TicketCounter(String counterName){
         this.counterName = counterName;
+        this.counterToBreak = new Random().nextInt(10)+1;
     }
 
     public String getCounterName(){
@@ -15,14 +21,25 @@ public class TicketCounter{
     public void sellTicket(Customer customer){
         synchronized (this){
             try{
-                Thread.sleep(new Random().nextInt(3) * 1000);
+                Thread.sleep(new Random().nextInt(2) * 1000);
             } catch(Exception e){}
             System.out.println("("+java.time.LocalTime.now().withNano(0) + " - " + getCounterName() + ")\t" + "Customer " + customer.getName() + " has reached " + getCounterName());
             try{
                 Thread.sleep(1000);
             } catch(Exception e){}
             customer.ticket = true;
+            servedCounter += 1;
             System.out.println("("+java.time.LocalTime.now().withNano(0) + " - " + getCounterName() + ")\t" + getCounterName() + " has sold a ticket to Customer " + customer.getName());
+            onBreak = servedCounter == counterToBreak ? true : false;
+            if(onBreak){
+                System.out.println("("+java.time.LocalTime.now().withNano(0)+") " +getCounterName() +" is having a TOILET BREAK, please wait for 5s");
+                try{
+                    Thread.sleep(5000);
+                } catch(Exception e){}
+                System.out.println("("+java.time.LocalTime.now().withNano(0)+") " +getCounterName() +" is ready to serve again");
+                onBreak = false;
+                servedCounter = 0;
+            }
         }
         if(customer.ticket == true){
             customer.ticketScanner.scanTicket(customer);
